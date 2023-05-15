75d2d11e9bd25a85b5f90540dabe852cdf6d8466
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2023-05-15 19:52:33 +0700

    first commit

---
+++ UTS-2.0/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using UTS_2._0.Models;
+
+namespace UTS_2._0;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+	private void OnCounterClicked1(object sender, EventArgs e)
+	{
+        Dosen dosen = new Dosen();
+        dosen.setDosen(123456, "John Doe");
+        dosen.setMatakuliah("Pemrograman Berorientasi Objek");
+        //Console.WriteLine($"Nik: {dosen.Nik}, Nama: {dosen.Nama}, Matakuliah: {dosen.Matakuliah}");
+
+		CounterBtn1.Text = $"Nik: { dosen.Nik}, Nama: { dosen.Nama}, Matakuliah: { dosen.Matakuliah}";
+
+        SemanticScreenReader.Announce(CounterBtn1.Text);
+    }
+	//public static void Program()
+	//{
+	//	Dosen dosen = new Dosen(123456, "John Doe");
+	//	dosen.SetDosen();
+	//	dosen.SetMatakuliah();
+	//	Console.WriteLine($"Nik: {dosen.Nik}, Nama: {dosen.Nama}, Matakuliah: {dosen.Matakuliah}");
+	//}
+}
---
+++ UTS-2.0/Models/Dosen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace UTS_2._0.Models
+{
+    public class Dosen
+    {
+        public string Matakuliah { get; set; }
+        public string Nama { get; set; }
+        public int Nik { get; set; }
+
+        public Dosen()
+        {
+        }
+
+        public void setDosen(int nik, string nama)
+        {
+            Nik = nik;
+            Nama = nama;
+        }
+
+        public void setMatakuliah(string matakuliah)
+        {
+            Matakuliah = matakuliah;
+        }
+    }
+}
+
